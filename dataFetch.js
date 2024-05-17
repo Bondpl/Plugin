7d102b7e5b3156d4eee455c2b11f4fe466881168
bond <rafal.majewski92@gmail.com>
2024-05-17 18:13:37 +0200

    added 3 days weather forecast and start using chart library

--- dataFetch.js
+++
@@ -1,9 +0,0 @@
-// const apiKey = process.env.MY_API_KEY;
-// console.log(apiKey)
-const config = require('./config');
-
-const apiKey = config.api_key;
-
-const res = fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=51.936619,15.508690`).then
-
-console.log(res)
